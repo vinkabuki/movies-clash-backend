91d3896822ae563bed14681c6d3ab56ce156ea0b
vinkabuki <vincent.kabacky@gmail.com>
2020-08-30 20:33:39 +0200

    init

---
+++ routes/api/accept.ts
@@ -0,0 +1,9 @@
+import express = require("express");
+const router: express.Router = express.Router();
+const db = require("./../../db.json");
+
+router.get("/", (req: express.Request, res: express.Response) => {
+	res.json(db.accept);
+});
+
+export default router;
--- server.ts
+++ server.ts
@@ -2,11 +2,15 @@ import express = require("express");
 import cors from "cors";
 
 import recommendations from "./routes/api/recommendations";
+import accept from "./routes/api/accept";
+import reject from "./routes/api/reject";
 
 const app: express.Application = express();
 
 app.use(cors());
 app.use("/recommendations", recommendations);
+app.use("/accept", accept);
+app.use("/reject", reject);
 
 const PORT = process.env.PORT || 5000;
 

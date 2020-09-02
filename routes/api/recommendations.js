bf694df50022a5b9530d928702c116bcb63c27f3
vinkabuki <vincent.kabacky@gmail.com>
2020-09-03 01:41:23 +0200

    added cors options

--- routes/api/recommendations.js
+++ routes/api/recommendations.js
@@ -4,18 +4,19 @@ var express = require("express");
 var router = express.Router();
 var db = require("./../../db.json");
 router.get("/", function (req, res) {
-    res.json(db.recommendations);
-    db.accept = [];
-    db.reject = [];
+	res.json(db.recommendations);
+	db.accept = [];
+	db.reject = [];
 });
+
 router.put("/:id/accept", function (req, res) {
-    db.accept.push(req.params.id);
-    console.log(db.accept);
-    console.log(db.reject);
+	db.accept.push(req.params.id);
+	console.log(db.accept);
+	console.log(db.reject);
 });
 router.put("/:id/reject", function (req, res) {
-    db.reject.push(req.params.id);
-    console.log(db.accept);
-    console.log(db.reject);
+	db.reject.push(req.params.id);
+	console.log(db.accept);
+	console.log(db.reject);
 });
 exports.default = router;
--- server.ts
+++ server.ts
@@ -5,9 +5,13 @@ import recommendations from "./routes/api/recommendations";
 import accept from "./routes/api/accept";
 import reject from "./routes/api/reject";
 
+const corsOptions = {
+	origin: "https://vinkabuki.github.io/movies-clash/",
+};
+
 const app: express.Application = express();
 
-app.use(cors());
+app.use(cors(corsOptions));
 app.use("/recommendations", recommendations);
 app.use("/accept", accept);
 app.use("/reject", reject);
